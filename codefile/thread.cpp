fdf3af2b168f5d62e4354db1c269004418535570
dai_qi_xiang <1290023174@qq.com>
2024-09-06 21:53:51 +0800

    update

--- codefile/thread.cpp
+++ codefile/thread.cpp
@@ -1,12 +1,27 @@
 //
 // Created by storm on 24-9-3.
 //
-#include "thread.h"
-void Ta(){while(1){cout<<1;}}
-void Tb(){while(1){cout<<2;}}
+#include <iostream>
+#include <thread>  // 使用标准库的线程功能
+using namespace std;
 
-int main(){
-    create(Ta);
-    create(Tb);
+// 定义线程执行的函数
+void Ta() {
+    while (1) {cout << 1;}
+}
 
+void Tb() {
+    while (1) {cout << 2;}
+}
+
+int main() {
+    // 创建两个线程，分别执行 Ta 和 Tb
+    thread t1(Ta);
+    thread t2(Tb);//因为创建了两个执行流，所以不会循环某一个函数，而是两个同时执行
+
+    // 等待两个线程完成
+    t1.join();
+    t2.join();
+
+    return 0;
 }
